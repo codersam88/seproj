0679c116a44eddda0075c34d7bd16b9f33038f79
mc13mt49 <mc13mt49@linux-ac73.site>
2014-02-26 22:32:53 +0530

    see through done

---
+++ src/binpacking/PMmodifier.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binpacking;
+
+/**
+ *
+ * @author mc13mt49
+ */
+public class PMmodifier {
+    private int PM_ID;
+    private PMmodifier[] PMlist;
+    private PMmodifier[] VMlist;
+    protected void addVM(int VM_ID, int cap){
+        
+    }
+    protected void addVM(int PM_ID,int VM_ID, int cap){
+        
+    }
+}
+
