bbbc5dc4be2f4da837aeb80dfc2cc7826abfda34
sampath <sampath.skt@gmail.com>
2014-03-22 17:44:50 +0530

    messy button

--- src/binpacking/BestInter.java
+++ src/binpacking/BestInter.java
@@ -4,6 +4,8 @@
  */
 package binpacking;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -16,10 +18,26 @@ public class BestInter {
     JFrame start;
     JButton lod;
     
-    private void Inint(){
+    BestInter(){
         start = new JFrame("Load GUI");
-        lod = new JButton("Load");
+        start.setLayout(new BorderLayout());
         
+        lod = new JButton("Load");
+        Dimension dim = new Dimension(500,400);
+        Dimension dim1 = new Dimension(10,5);
+        lod.setSize(dim1);
+        start.add(lod,BorderLayout.CENTER);
+        start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        start.setSize(dim);
+        start.setVisible(true);
+        
+        
+        
+    }
+    
+    public static void main(String[] args){
+        
+        BestInter b = new BestInter();
     }
     
 }
