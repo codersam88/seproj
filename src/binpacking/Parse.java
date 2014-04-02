045992ddeb4ec25e1961a6699a9c517916191e54
mc13mt49 <mc13mt49@linux-ac73.site>
2014-04-02 20:38:58 +0530

    recursive scan

--- src/binpacking/Parse.java
+++ src/binpacking/Parse.java
@@ -24,14 +24,22 @@ public class Parse {
     void parseFile() throws IOException{
         Path  fPath = Paths.get("test.txt");
         Scanner scan = new Scanner(fPath);
-        scan.useDelimiter("|");
-        System.out.println(scan.next()+scan.next()+scan.next()+scan.next()+scan.next());
-        /*scan.useDelimiter("|");
-        for(;scan.hasNext();){
-            String VM_ID = scan.next();
-            int cap = Integer.parseInt(scan.next());
-            System.out.println(VM_ID+" "+cap);
-        }*/
+        //scan.useDelimiter("|");
+        for(;scan.hasNextLine();){
+                parseCurrLine(scan.nextLine());
+        }
         
     }
+    
+    void parseCurrLine(String li){
+        Scanner scanL = new Scanner(li);
+        scanL.useDelimiter("|");
+        if(scanL.hasNext()){
+            String VM_ID = scanL.next();
+            String cap = scanL.next();
+            System.out.println(VM_ID+" "+cap);
+            
+        }
+    
+}
 }
